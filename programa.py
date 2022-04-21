d0289445538ecb77408d8e1f723f95e8e2217047
JulianEstebanHerrera <julianesteban.herrera@mercadolibre.com.co>
2022-04-21 12:14:12 -0500

    comentario programa.py

--- programa.py
+++ programa.py
@@ -2,11 +2,9 @@
 
 if __name__=="__main__":
     print("hola")
-<<<<<<< HEAD
     print("MELI")
-=======
 
     print("detached head mode")
->>>>>>> newBranch
 
 print("comentario")
+print("comentario2")
